de1af1ec1c4af9bdac760cf743e025132fb1fea0
daichi-33 <knz.bgnl@gmail.com>
2021-07-17 17:28:24 +0900

    sneakerのモデル・コントローラー・ビュー・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_071646) do
+ActiveRecord::Schema.define(version: 2021_07_17_073550) do
+
+  create_table "sneakers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sneakers_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username", null: false
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_07_17_071646) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sneakers", "users"
 end
