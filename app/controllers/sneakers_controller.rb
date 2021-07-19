63c706eda10565c6bc196e294b3a17bbd474cde6
daichi-33 <knz.bgnl@gmail.com>
2021-07-19 09:31:32 +0900

    Active Strageの導入

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,7 +1,22 @@
 class SneakersController < ApplicationController
   def index
+    @sneakers = Sneaker.order('created_at DESC')
+  end
+
+  def new
   end
 
   def create
+    @sneaker = sneaker.new(sneaker_params)
+    if @sneaker.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def sneaker_params
+    params.require(:sneaker).permit(:title, :image, :detail).merge(user_id: current_user.id)
   end
 end
